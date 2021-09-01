770f14ba922bb44206eb904f2c11dc76e55957aa
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2021-09-01 11:15:45 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
 
 import indigo from "@material-ui/core/colors/indigo";
 import createTheme from "@material-ui/core/styles/createTheme";
 import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
-
+import NavBar from "./components/NavBar";
+import ApiContextProvider from "./context/ApiContext";
+import Main from "./components/Main";
 
 const theme = createTheme({
   palette: {
@@ -19,8 +21,13 @@ const theme = createTheme({
 
 function App() {
   return (
+    // ApiContextProviderで囲いをすることでApiContextの中の実装した関数を使用することができる
+    <ApiContextProvider>
       <MuiThemeProvider theme={theme}>
+        <NavBar />
+        <Main />
       </MuiThemeProvider>
+    </ApiContextProvider>
   );
 }
 
